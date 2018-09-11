062310bd714794f9f3ff2f5eacbd207572c262ba
vinod g chandaliya <vinodgchandaliya@gmail.com>
2018-09-11 21:54:51 +0530

    basic form created for csv upload

---
+++ server.js
@@ -0,0 +1,17 @@
+/* csv to json */
+
+function getcsv() {
+  console.log("csv data check");
+}
+
+function clearData() {
+  console.log("data cleared");
+}
+
+function convertData() {
+  console.log("convert data to json");
+}
+
+function formatData() {
+  console.log("format data as per requirement");
+}
