317a652c958f602a58dde4cb2faad4f981fa49eb
vinod g chandaliya <vinodgchandaliya@gmail.com>
2018-09-11 22:32:14 +0530

    http module added

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 /* csv to json */
+
+/** @package @constant express is imported and
+ * @constant cjApp is used for get post api request */
 const express = require ('express');
 const cjApp = express(); 
 
+/** @constant http is used to host local server on port 8009  */
 const http = require('http').Server(cjApp).listen(8009)
 console.log('server started on port 8009')
 
-
+/* get request to send default html file for file upload */
 cjApp.get('/', (req, res, next)=>{
   res.sendFile(__dirname+'/index.html')
 })
