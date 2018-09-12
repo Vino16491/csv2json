731907c7dac8671c89147f0b8bccc9e2774a5616
vinod g chandaliya <vinodgchandaliya@gmail.com>
2018-09-12 13:57:34 +0530

    csv to json conversion succesful

--- server.js
+++ server.js
@@ -1,39 +1,25 @@
 /* csv to json */
 
-const express = require ('express'),
-      app = express(),
-      http = require('http'),
-      upload = require('express-fileupload'),
-      fs = require('fs');
-let jsonData = 'test';
+const express = require("express"),
+  app = express(),
+  upload = require("express-fileupload"),
+  csvtojson = require("csvtojson"),
+  fs = require("fs");
+let jsonData = "test";
 app.use(upload());
 
-app.get('/', (req, res, next)=>{
-  res.sendFile(__dirname+'/index.html')
-})
+app.get("/", (req, res, next) => {
+  res.sendFile(__dirname + "/index.html");
+});
 
-app.post('/file',(req, res)=>{
-  res.send(jsonData);
-})
+app.post("/file", (req, res) => {
+  jsonData = req.files.csvfile.data.toString('utf8');
+  return csvtojson().fromString(jsonData).then(json => {
+    res.status(201).json({ ...json
+    })
+  })
+});
 
-function getcsv(csvfile) {
-  console.log("csv data check" + csvfile);
-  x = csvfile;
-  document.getElementById('csvfiletoJson').value = x;
-
-  return false;
-}
-
-function clearData() {
-  console.log("data cleared");
-}
-
-function convertData() {
-  console.log("convert data to json");
-}
-
-function formatData() {
-  console.log("format data as per requirement");
-}
-http.Server(app).listen(8009)
-console.log('server started on port 8009')
+app.listen(process.env.PORT || 4000, function(){
+  console.log('Your node js server is running');
+});
