705e37a83da29a60f3519a7b3402fe92f74fad29
vinod g chandaliya <vinodgchandaliya@gmail.com>
2018-10-22 05:47:05 +0530

    CORS updated

--- server.js
+++ server.js
@@ -7,7 +7,18 @@ const express = require("express"),
   
 let csvData = "test";
 app.use(upload());
-
+app.use(function(req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "POST, GET, OPTIONS"
+  );
+  next();
+});
 app.get("/", (req, res, next) => {
   res.sendFile(__dirname + "/index.html");
 });
